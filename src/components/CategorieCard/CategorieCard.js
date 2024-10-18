65743b08b4c7a67c2d58e45f21cd40da0df2ba2a
Jadla Ennih <Eljadla.Ennih@outlook.fr>
2024-10-18 16:11:10 +0200

    ajout class

--- src/components/CategorieCard/CategorieCard.js
+++ src/components/CategorieCard/CategorieCard.js
@@ -6,7 +6,7 @@ const CategorieCard = ({ name, description }) => {
     return (
         <div className="categorie-card">
             {/*lien vers la page spécifique d'un sport, avec le nom du sport en minuscule dans l'URL */}
-            <Link to={`/sports/${name.toLowerCase()}`}>{name}</Link>
+            <Link className='taille-a' to={`/sports/${name.toLowerCase()}`}>{name}</Link>
             <p>{description}</p>
         </div>
     );
